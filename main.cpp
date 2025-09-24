4579402d25c36acded8e68b60392dc7946c32421
chupep03 <finogenovnikita.2007@gmail.com>
2025-09-24 23:51:47 +0300

    Finogenov Nikita

--- main.cpp
+++ main.cpp
@@ -1,79 +1,113 @@
-#include <cstdio>
 #include <iostream>
-#include <cstring>
 #include <fstream>
-#include <getopt.h>
-#include <cctype>
+#include <cstring>
 
-int main(int argc, char *argv[]) {
-    char* filename = nullptr;
-    char* word = nullptr;
-    
-    int opt;
-    while((opt = getopt(argc, argv, "f:w:")) != -1) {
-        switch (opt) {
-            case 'f':
-                filename = optarg;
+bool check_word(char* word, char* word_for_check) {
+
+    for(int i = 0; i < strlen(word_for_check); i++) {
+        bool is_found = false;
+        for(int j = 0; j < strlen(word); j++) {
+            if(word_for_check[i] == word[j]) {
+                is_found = true;
                 break;
-            case 'w':
-                word = optarg;
-                break;
-            default:
-                std::cout << "Usage:" << argv[0] << " -f <filename> -w <word>" << std::endl;
-                return 1;
+            }
+        }
+        if(!is_found) {
+            return false;
         }
     }
-    
-    if (!filename || !word) {
-        std::cout << "Usage:" << argv[0] << " -f <filename> -w <word>" << std::endl;
-        return 1;
-    }
-    
+    return true;
+}
+
+int check_all_words(char* word, char* PATH, bool need_to_print){
+
     std::ifstream myfile;
-    myfile.open(filename, std::ios::binary | std::ios::ate);
-    if (!myfile.is_open()) {
-        std::cout << "Can't read a file" << std::endl;
+    myfile.open(PATH);
+
+    char files_word[33];
+    char current_ch;
+    int index = 0;
+    int count = 0;
+
+    if(!myfile.is_open()) {
+        std::cerr << "Can't read a file" << std::endl;
+        return -1;
+    }
+
+    while(myfile.get(current_ch)) {
+        if(current_ch == ' ' || current_ch == '\n' || current_ch == '\0') {
+            if(index > 0) {
+                files_word[index] = '\0';
+                if(check_word(files_word, word)) {
+                    count++;
+                    if(need_to_print){
+                       std::cout << count <<". " << files_word << std::endl; 
+                    }
+                    
+                }
+                index = 0; 
+            }
+        } 
+        else {
+            if(index < 32){
+                files_word[index++] = current_ch;
+            }
+        }
+    }
+    myfile.close();
+    return count;
+}
+
+int find_words_count(char* word, char* PATH, bool need_to_print = 1) {
+    if(need_to_print){
+        std::cout << "##############" << std::endl;
+    }
+    int count = check_all_words(word, PATH, need_to_print);
+    if(need_to_print){
+        std::cout << "##############" << std::endl;
+    }
+    return count;
+}
+
+int main(int argc, char *argv[]) {
+    char* word = nullptr;
+    char* filename = nullptr;
+    
+    if(argc < 5) {
+        std::cerr << argv[0] << " -f <file_name> -w <word_to_find>" << std::endl;
         return 1;
     }
-
-    int size = myfile.tellg();
-    char* textFile = new char[size+1];
-
-    myfile.seekg(0);
-    myfile.read(textFile, size);
-    textFile[size] = '\0';
-
-    int left = 0;
-    int right;
-
-    for(int i = 0; i<size; i++){
-        if(textFile[i] == ' ' || textFile[i] == '\n' || textFile[i] == '\0'){
-            bool needToPrint = true;
-            right = i-1;
-
-            for(int w = 0; w < strlen(word); w++){
-                bool isInTextWord = false;
-                for(int j = left; j<=right; j++){
-                    if(tolower(word[w]) == tolower(textFile[j])){
-                        isInTextWord = true;
-                    }
-                }
-                if(!isInTextWord){
-                    needToPrint = false;
-                    break;
-                }
+    
+    for(int i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "-w") == 0) {
+            if(i + 1 < argc) {
+                word = argv[i + 1];
+                i++;
+            } else {
+                std::cerr << "Error: Missing value for -w flag" << std::endl;
+                return 1;
             }
-            if(needToPrint){
-                for(int j = left; j<=right; j++){
-                    std::cout << textFile[j];
-                }
-                if(textFile[i] != '\0'){
-                    std::cout << " ";
-                }
-            }
-            left = i+1;
         } 
+        else if(strcmp(argv[i], "-f") == 0) {
+            if(i + 1 < argc) {
+                filename = argv[i + 1];
+                i++;
+            } else {
+                std::cerr << "Error: Missing value for -f flag" << std::endl;
+                return 1;
+            }
+        }
+        else {
+            std::cerr << "Warning: Unknown argument " << argv[i] << std::endl;
+        }
     }
-    delete[] textFile;
+
+    std::cout << "word: " << word << " | " << "file: "<< filename << std::endl;
+    int result = find_words_count(word, filename, 1);
+    
+    if(result >= 0) {
+        std::cout << "Number of words with '" << argv[2] << "' string: " << result << std::endl;
+    }
+    
     return 0;
 }
